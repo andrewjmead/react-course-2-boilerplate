46cf882d2ceb96f60b27634bf8c7384c492f6f74
Andrew Mead <andrewjosephmead@gmail.com>
2017-11-10 08:54:39 -0500

    Update to React v16

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,1 +1,6 @@
-require('dotenv').config({ path: '.env.test' });
+import Dotenv from 'dotenv';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Dotenv.config({ path: '.env.test' });
+Enzyme.configure({ adapter: new Adapter() });
